4a6293caf61fa3ae56e19146220dfd6f3f15d0a6
Stoic <112611821+TheStoicBear@users.noreply.github.com>
2024-07-27 09:37:02 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yyes'
+author 'TheStoicBear'
+description 'Stoic-Advanced Vehicle System'
+version '1.0.0'
+
+client_scripts {
+    '@ox_lib/init.lua',
+    'config.lua',
+    'cl.lua'
+}
+server_script 'sv.lua'
